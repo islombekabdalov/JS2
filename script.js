284c4c1f030ab01c90fcdc4e91f8f81a14a3545c
Islom <Islom@gmail.com>
2022-11-18 18:19:32 +0500

    JS2

---
+++ script.js
@@ -0,0 +1,77 @@
+var ism = prompt("Ismingizni kiriting")
+console.log("Meni ismim "+ ism);
+
+var yosh =+prompt("Sizning yoshingiz")
+console.log( "Men  "+ yosh + " yoshdaman");
+
+var misol1 =+prompt("misloni yeching: " +"6+6=?")
+if ( misol1 ==  12 ){
+    console.log( "Siz togri javob berdingiz: " + misol1 )
+} else{
+    console.log( "Siz notogri javob berdingiz: " + misol1 + "; Togri javob: 12")
+}
+
+var misol2 =+prompt("misloni yeching: " +"10-4=?")
+if ( misol2 ==  6 ){
+    console.log( "Siz togri javob berdingiz: " + misol2 )
+} else{
+    console.log( "Siz notogri javob berdingiz: " + misol2 + "; Togri javob: 6")
+}
+
+var misol3 =+prompt("misloni yeching: " +"2*2=?")
+if ( misol3 ==  4 ){
+    console.log( "Siz togri javob berdingiz: " + misol3 )
+} else{
+    console.log( "Siz notogri javob berdingiz: " + misol3 + "; Togri javob: 4")
+}
+
+var misol4 =+prompt("misloni yeching: " +"6/2=?")
+if ( misol4 ==  3 ){
+    console.log( "Siz togri javob berdingiz: " + misol4 )
+} else{
+    console.log( "Siz notogri javob berdingiz: " + misol4 + "; Togri javob: 3")
+}
+
+var misol5 =+prompt("misloni yeching: " +"10%3=?")
+if ( misol5 ==  1 ){
+    console.log( "Siz togri javob berdingiz: " + misol5 )
+} else{
+    console.log( "Siz notogri javob berdingiz: " + misol5 + "; Togri javob: 1")
+}
+
+var misol6 =+prompt("misloni yeching: " +"652+566=?")
+if ( misol6 == 1218  ){
+    console.log( "Siz togri javob berdingiz: " + misol6 )
+} else{
+    console.log( "Siz notogri javob berdingiz: " + misol6 + "; Togri javob: 1218")
+}
+
+var misol7 =+prompt("misloni yeching: " +"10*5=?")
+if ( misol7 ==  50 ){
+    console.log( "Siz togri javob berdingiz: " + misol7 )
+} else{
+    console.log( "Siz notogri javob berdingiz: " + misol7 + "; Togri javob: 50")
+}
+
+var misol8 =+prompt("misloni yeching: " +"50/5=?")
+if ( misol8 ==  10 ){
+    console.log( "Siz togri javob berdingiz: " + misol8 )
+} else{
+    console.log( "Siz notogri javob berdingiz: " + misol8 + "; Togri javob: 10")
+}
+
+var misol9 =+prompt("misloni yeching: " +"153%5=?")
+if ( misol1 ==  1 ){
+    console.log( "Siz togri javob berdingiz: " + misol9 )
+} else{
+    console.log( "Siz notogri javob berdingiz: " + misol9 + "; Togri javob: 1")
+}
+
+var misol10 =+prompt("misloni yeching: " +"99-1=?")
+if ( misol10 ==  98 ){
+    console.log( "Siz togri javob berdingiz: " + misol10 )
+} else{
+    console.log( "Siz notogri javob berdingiz: " + misol10 + "; Togri javob: 88")
+}
+
+alert('Javobini bilish uchun consolega kiring');
